aa892cac0f52b8f569fa5348deec789f29db6df4
Brent Morris <bmorris@algal.ca>
2024-07-31 16:19:08 -0400

    boilerplate slider code from stack overflow

--- sticky-header.js
+++ sticky-header.js
@@ -26,3 +26,31 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+/* Next I want to make an infinate slider as explined here https://stackoverflow.com/questions/73925164/how-to-make-a-slider-loop-with-js */
+
+function sliderFunc() {
+    let sliderBox = document.querySelector('.slider');
+    let tapToRightBtn = document.querySelector('.btn');
+    let index = 0;
+    sliderBox.style.width = '500px';
+    sliderBox.style.left = '0px';
+  
+    function sliderNext() {
+      setTimeout(() => {
+        index++;
+        let child = sliderBox.querySelector(`div:nth-child(${index})`);
+        let cloneNode = child .cloneNode(true);
+        sliderBox.style.width = `${(5 + index) * 100}px`;
+        sliderBox.appendChild(cloneNode);
+      }, 300);
+      // clone and move the element to bottom
+      let currentLeftPosition = sliderBox.style.left ? parseFloat(sliderBox.style.left.replace('px', '')) : 0;
+      let nextLeftPosition = currentLeftPosition - 100;
+      sliderBox.style.left = `${nextLeftPosition}px`;
+  
+    }
+    tapToRightBtn.addEventListener('click', sliderNext);
+  }
+  sliderFunc();
