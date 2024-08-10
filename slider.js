321f26c4dd997fc7cee29a11b0db2ad990964ece
Brent Morris <brent.morris.2165@gmail.com>
2024-08-10 16:18:40 -0400

    Project slide width spaced nicley

--- slider.js
+++ slider.js
@@ -1,3 +1,7 @@
+/* This code is responsible for the project infinite slider section.
+Three projects will show regardless of the aspect ratio of the view window */
+
+
 document.addEventListener("DOMContentLoaded", function() {
     let container = document.querySelector('.container');
     let sliderBox = document.querySelector('.slider');
@@ -12,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
         handleResize();//dimention slider panels as soon as document opens
 
         //Create coppies of the first 3 slides and append them to the end of the felxbox
+        //these copys are used to create the looping effect
         let firstSlide = slides[0].cloneNode(true);
         let secondSlide = slides[1].cloneNode(true);
         let thirdSlide = slides[2].cloneNode(true);
@@ -39,11 +44,18 @@ document.addEventListener("DOMContentLoaded", function() {
     // Function to run on window resize
     function handleResize() {
     // set slide width so 3 fit on the screen
-    slideWidth = (container.offsetWidth * 0.334);
+    slideWidth = (container.offsetWidth * 0.306667);
+    marginWidth = (container.offsetWidth *0.0101) // the 0.0001 prevents the 4th slide from bleeding over in some circumstances
     slides.forEach(slide => {
-        slide.style.offsetWidth = slideWidth + 'px';
+        // do not use this code because it includes the region hidden by the scroll bar in the viewport width
+        //slide.style.width = '30.6666vw';
+        //slide.style.margin = '1vw';
+        slide.style.width = slideWidth + 'px';
+        slide.style.margin = marginWidth + 'px';
     });
-    sliderBox.style.width = ((slides.length + 2) * slideWidth) + 'px'; //replace slide width * 7 with an expression that varries with the number of slides
+    //No harm in having the width of the slider box larger than it needs to be
+    //if the box is too small it will shrink the project slides from 33% of screen width
+    sliderBox.style.width = ((slides.length + 10) * slideWidth) + 'px'; //replace slide width * 7 with an expression that varries with the number of slides
 }
 
 
